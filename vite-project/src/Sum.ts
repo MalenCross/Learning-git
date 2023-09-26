4b6924c007c170f3e8c863eeaaaa720eb0da349f
Malen Cross <MalenCross@yahoo.com>
2023-09-25 22:52:42 -0500

    one problem left to fix

--- vite-project/src/Sum.ts
+++ vite-project/src/Sum.ts
@@ -1,3 +1,15 @@
 export function Sum(x:number, y:number) :number {
 	return x + y;
 }
+
+export function Div(x:number, y:number) :number {
+	return x/y;
+}
+
+export function SumOfSquares(x:number, y:number) :number {
+	return x + y;
+}
+
+export function Greeting(name:string) :string {
+	return "Hello " + name;
+}

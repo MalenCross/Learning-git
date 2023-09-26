632310eef620adc2ad492d7286271eee3d45804e
Malen Cross <MalenCross@yahoo.com>
2023-09-25 23:01:41 -0500

    found the problem
    turns out it was a trick question! the froblem was in the test. 2 squared plus 4 squared equls 20 not 18.

--- vite-project/src/Sum.ts
+++ vite-project/src/Sum.ts
@@ -7,7 +7,7 @@ export function Div(x:number, y:number) :number {
 }
 
 export function SumOfSquares(x:number, y:number) :number {
-	return x + y;
+	return Math.pow(x,2) + Math.pow(y,2);
 }
 
 export function Greeting(name:string) :string {
